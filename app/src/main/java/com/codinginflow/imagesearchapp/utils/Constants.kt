85b230796cbe621b6bfa14dcce81ba0fae3fcab4
ImVikasJi <vbhadoriya3@gmail.com>
2021-09-24 07:47:01 +0530

    Added UnsplashApi.kt , UnsplashResponse.kt and Constants.kt

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.codinginflow.imagesearchapp.utils
+
+import com.codinginflow.imagesearchapp.BuildConfig
+
+object Constants {
+    const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
+    const val BASE_URL = "https://api.unsplash.com/"
+}
